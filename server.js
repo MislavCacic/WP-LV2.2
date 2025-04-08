bceab6cc4239ca03b32f356ab2e673e6af792003
Mislav Čačić <mislavcacic@Mislavs-MacBook-Pro.local>
2025-04-08 22:12:29 +0200

    treći zadatak

--- server.js
+++ server.js
@@ -1,13 +1,30 @@
 const express = require("express");
 const app = express();
+const PORT = process.env.PORT || 3000;
+const fs = require("fs");
+const path = require("path");
 
-app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+
+app.use(express.static(path.join(__dirname, "public")));
 
 app.get("/", (req, res) => {
-	res.send("Ili obican tekst ako nema HTML datoteke.");
+	res.redirect("/slike");
 });
 
-const PORT = process.env.PORT || 3000;
+app.get("/slike", (req, res) => {
+	fs.readFile(path.join(__dirname, "images.json"), "utf-8", (err, data) => {
+		if (err) {
+			return res.status(500).send("Error reading images data.");
+		}
+
+		const images = JSON.parse(data);
+
+		res.render("slike", { images });
+	});
+});
+
 app.listen(PORT, () => {
 	console.log(`Server pokrenut na portu ${PORT}`);
 });
